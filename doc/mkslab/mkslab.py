4bf6dd2b8ea75c6a1d0635053235fc9cbd850a8a
Christian Meisenbichler <christian.meisenbichler@unileoben.ac.at>
2011-11-07 16:22:16 +0100

    debug mkslab

--- doc/mkslab/mkslab.py
+++ doc/mkslab/mkslab.py
@@ -1,6 +1,9 @@
 from stool.mkslab import slab
 from ase import read,write
-bulk=read('../Elastic/AlBulk.xml',format='exi')
-slab=slab(bulk,layers=3,miller=[1,-2,-2])
+bulk=read('AlBulk.xml',format='exi')
+
+slab=slab(bulk,layers=3,miller=[1,1,0],vacuum=10)
  
 write('slab.pov',slab, display=False, run_povray=True)
+write('slab.xsf',slab)
+write('bulk.xsf',bulk)
